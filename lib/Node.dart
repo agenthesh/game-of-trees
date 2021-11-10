bfe614f1fd91e91f40c4a3ba85ca8a0de20f0525
Hussain Surti <surti.huss@gmail.com>
2021-11-10 10:59:12 +0400

    Extended Comparable for comparison and sorting

--- lib/Node.dart
+++ lib/Node.dart
@@ -6,20 +6,31 @@ class Graph {
   Graph(this.nodes);
 }
 
-class Node {
-  final String id;
-  final int data;
+class Node implements Comparable<dynamic> {
+  final String label;
   final Vector2 positionOnGrid;
 
-  Node(this.id, this.data, this.positionOnGrid);
+  Node({required this.label, required this.positionOnGrid});
 
   @override
   bool operator ==(Object other) =>
-      other is Node && other.positionOnGrid == positionOnGrid;
+      (other is Node) && (other.positionOnGrid == positionOnGrid);
 
   @override
   int get hashCode => positionOnGrid.hashCode;
 
   @override
-  String toString() => '$id';
+  String toString() => '$label';
+
+  @override
+  int compareTo(other) {
+    if (other is Node) {
+      if (this.positionOnGrid.x > other.positionOnGrid.x &&
+          this.positionOnGrid.y > other.positionOnGrid.y) {
+        return 1;
+      }
+      return 0;
+    }
+    return 0;
+  }
 }
