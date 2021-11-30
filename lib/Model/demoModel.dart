89e7bcfb680fcbd8a2ead9bb07c06067ccdb96b0
Hussain Surti <surti.huss@gmail.com>
2021-11-30 23:11:05 +0400

    created demo data for testing purposes

---
+++ lib/Model/demoModel.dart
@@ -0,0 +1,20 @@
+const Map<String, dynamic> DEMO_CHAR_VECTORS = {
+  "nodes": [
+    {
+      "numberOfNodes": 5,
+      "characteristicVectors": [
+        {"L0": 5, "L1": 4, "L2": 3, "L3": 2, "L4": 1, "L5": 0},
+        {"L0": 5, "L1": 4, "L2": 4, "L3": 2, "L4": 0, "L5": 0},
+        {"L0": 5, "L1": 4, "L2": 6, "L3": 0, "L4": 0, "L5": 0}
+      ]
+    },
+    {
+      "numberOfNodes": 6,
+      "characteristicVectors": [
+        {"L0": 5, "L1": 4, "L2": 3, "L3": 2, "L4": 1, "L5": 0, "L6": 0},
+        {"L0": 5, "L1": 4, "L2": 4, "L3": 2, "L4": 0, "L5": 0, "L6": 0},
+        {"L0": 5, "L1": 4, "L2": 6, "L3": 0, "L4": 0, "L5": 0, "L6": 0}
+      ]
+    }
+  ]
+};
