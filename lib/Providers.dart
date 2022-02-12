cffa42b5b7ebb58992d3e97566d36d2f753f6059
Hussain Surti <surti.huss@gmail.com>
2022-02-12 15:09:42 +0400

    added new providers

--- lib/Providers.dart
+++ lib/Providers.dart
@@ -1,14 +1,16 @@
-import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:game_of_trees/Model/levelData.dart';
 
 final remainingNodeProvider = StateProvider<int>((ref) {
-  return 5;
+  return 0;
 });
 
-final cvProvider = StateProvider<String>((ref) {
-  return "";
+final cvProvider = StateProvider<Map<String, int>>((ref) {
+  return {};
 });
 
 final cvCheckProvider = StateProvider<bool>((ref) {
   return false;
 });
+
+final levelDataProvider = ChangeNotifierProvider((ref) => LevelData());
